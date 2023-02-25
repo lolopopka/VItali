c7f32b7f7162185d638f25bcf8c525c7bdc60c6a
Vitali <boronenckovitali@yandex.ru>
2023-02-26 00:15:53 +0300

    ok

--- Hw4/3155/main.py
+++ Hw4/3155/main.py
@@ -1,9 +1,6 @@
 line = input().split()
-k = 0
-for i in range(len(line)):
-    k = k + 1
-    result = line[k]
-    if int(line[i]) < int(result):
-        print(result)
-
-#я не знаю как исправить ошибку
+for k in range(len(line)):
+    line[k] = int(line[k])
+for i in range(1, len(line)):
+    if line[i] > line[i - 1]:
+        print(line[i], end= ' ')
